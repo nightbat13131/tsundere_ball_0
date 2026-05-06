f3144c63e0279ea5f918c794a321640dbb23cccf
Cole H <nightbat13131@gmail.com>
2026-05-06 15:38:43 -0700

    starting player_ball state machine

--- balls/ball_player_withwalk.gd
+++ balls/ball_player_withwalk.gd
@@ -13,10 +13,15 @@ const MIN_POWER := MAX_POWER * .05
 
 const LEFT_MOUSE_CLICK = &"left_click"
 
+const EVENT_MOUSE_PRESSED = &'mouse_pressed'
+const EVENT_MOUSE_RELEASED = &'mouse_released'
+
 var cancle_pause := 0.0
 var cycle_mod := 1
 
 static var _instance : Ball_Player_Walking
+
+@onready var state_chart: StateChart = %StateChart
 
 @export_category("G.U.I.D.E.")
 @export var pc_controler_context: GUIDEMappingContext
@@ -24,14 +29,6 @@ static var _instance : Ball_Player_Walking
 @export var action_cancle_ball: GUIDEAction
 @export var action_mouse_pressed : GUIDEAction
 @export var action_mouse_release : GUIDEAction
-
-var _power := MIN_POWER :
-	set(value):
-		_power = value
-		if _power > MAX_POWER and cycle_mod == 1:
-			cycle_mod = -1
-		elif _power < MIN_POWER and cycle_mod == -1:
-			cycle_mod = 1
 
 var global_mouse_start := DEFAULT_POS
 var global_mouse_end := DEFAULT_POS
@@ -44,12 +41,11 @@ func _ready() -> void:
 	set_y_sort_enabled(false) 
 	if pc_controler_context:
 		GUIDE.enable_mapping_context(pc_controler_context)
-		if action_mouse_pressed == null:
-			push_error("no action_mouse_pressed")
-		if action_mouse_release == null:
-			push_error("no action_mouse_release")
-		if action_cancle_ball == null:
-			push_error("no action_cancle_ball")
+		if action_mouse_pressed:
+			action_mouse_pressed.triggered.connect(_send_event.bind(EVENT_MOUSE_PRESSED))
+		if action_mouse_release:
+			action_mouse_release.triggered.connect(_send_event.bind(EVENT_MOUSE_RELEASED))
+
 	set_collision_layer_value(Ball.LAYER_PC, true)
 	set_collision_mask_value(Ball.LAYER_PC_WALL, true)
 	_set_shader_parameter(UTILITIES.SHADER_OUTLINE_COLOR, UTILITIES.COLOR_BORDER_BOUNCY)
@@ -68,41 +64,14 @@ func _get_power_ratio() -> float:
 	#prints(_power, _get_usable_power(),  MAX_POWER, _get_usable_power() / MAX_POWER)
 	return _get_usable_power() / MAX_POWER
 
-func _process(delta: float) -> void:
-	super._process(delta)
-	
-	if action_cancle_ball.is_triggered():
-		_cancle_shoot()
-		return
-	if cancle_pause > 0.0:
-		cancle_pause -= delta
-		return
-	_rolling_movement(delta)
-
-func _rolling_movement(_delta) -> void:
-	queue_redraw()
-	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		global_mouse_end = get_global_mouse_position()
-		if global_mouse_start == DEFAULT_POS:
-			global_mouse_start = global_mouse_end
-	else:
-		if global_mouse_start != DEFAULT_POS and global_mouse_end != DEFAULT_POS:
-			_shoot_0()
 
 func get_shot_angle_vector() -> Vector2:
 	if global_mouse_start != DEFAULT_POS and global_mouse_end != DEFAULT_POS:
 		return global_mouse_start - global_mouse_end
 	return DEFAULT_POS
 
-func _shoot_0() -> void:
-	apply_central_impulse(get_shot_angle_vector().normalized() * _get_usable_power())
-	_power = MIN_POWER
-	global_mouse_end = DEFAULT_POS
-	global_mouse_start = DEFAULT_POS
 
 func _cancle_shoot() -> void:
-	_power = 0.0
 	global_mouse_end = DEFAULT_POS
 	global_mouse_start = DEFAULT_POS
 	cancle_pause = CANCLE_PAUSE_DURATION
@@ -126,3 +95,42 @@ func _draw() -> void:
 	draw_polygon(
 		[direction*visable_power, direction.rotated(- PI*.5)*BALL_RADIUS, direction.rotated( PI*.5)*BALL_RADIUS,  ], [color_power]
 			)
+
+func _send_event(event: String) -> void:
+	print(event)
+	if state_chart:
+		state_chart.send_event(event)
+	else:
+		push_error(self, " has no State Chart")
+
+
+func _on_waiting_walk_state_processing(delta: float) -> void:
+	pass # Replace with function body.
+
+func _on_waiting_roll_state_processing(delta: float) -> void:
+		
+	if action_cancle_ball.is_triggered():
+		_cancle_shoot()
+		return
+	if cancle_pause > 0.0:
+		cancle_pause -= delta
+		return
+	_rolling_movement(delta)
+
+
+
+
+
+func _on_aiming_state_entered() -> void: global_mouse_start = get_global_mouse_position()
+
+func _on_aiming_state_exited() -> void: global_mouse_end = get_global_mouse_position()
+
+
+func _on_try_shoot_state_entered() -> void: 
+	apply_central_impulse(get_shot_angle_vector().normalized() * _get_usable_power())
+	global_mouse_end = DEFAULT_POS
+	global_mouse_start = DEFAULT_POS
+
+
+func _on_aiming_state_processing(delta: float) -> void:
+	pass # Replace with function body.
