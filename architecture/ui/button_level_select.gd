0472c5dd63754b241a1629d4679da5ff152e3bef
Cole H <nightbat13131@gmail.com>
2026-05-09 15:02:50 -0700

    making props

--- architecture/ui/button_level_select.gd
+++ architecture/ui/button_level_select.gd
@@ -3,16 +3,18 @@ class_name ButtonLevelSelect extends ButtonSelf
 # TODO: enter level button cursors 
 
 const PATH_FACE = 'uid://dp6bw3nvdfa4g'
-const PATH_CURSOR_ENTER_LEVEL = ''
 static var master_face : FaceTexture
+
+const PATH_CURSOR_ENTER_LEVEL = 'uid://bgs66genfal2j'
+const PATH_CURSOR_LEVEL_BLOCKED = 'uid://dyrjqnpjmmggu'
+static var cursor_enter_level : CustomCursor
+static var cursor_level_blocked : CustomCursor
 
 @export var level_info : LevelInfo
 var _face : FaceTexture
 
 func _ready() -> void:
 	super._ready()
-	#_face = load(PATH_FACE)
-	#_face = _face.duplicate(true)
 	set_button_icon(get_face())
 	if level_info != null:
 		level_info._ready()
@@ -23,13 +25,23 @@ func _ready() -> void:
 
 func _on_pressed() -> void:
 	if level_info:
-		LevelSelect.request_level(level_info)
+		if !level_info.is_locked():
+			LevelSelect.request_level(level_info)
 	else:
 		push_warning("Level Select button has no Level Info")
 
 func _on_level_info_change() -> void:
-	print(self)
 	get_face().set_emotion(level_info.get_emotion())
+	if cursor_enter_level == null:
+		cursor_enter_level = load(PATH_CURSOR_ENTER_LEVEL)
+	if cursor_level_blocked == null:
+		cursor_level_blocked = load(PATH_CURSOR_LEVEL_BLOCKED)
+	if level_info.is_locked():
+		if cursor_level_blocked:
+			cursor_level_blocked.apply_to(self)
+	else:
+		if cursor_enter_level:
+			cursor_enter_level.apply_to(self)
 
 func get_face() -> FaceTexture:
 	if master_face == null:
