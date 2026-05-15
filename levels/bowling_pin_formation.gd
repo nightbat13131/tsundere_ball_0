45e1d6d36d5de806af423e93bed81cb610dcfbc1
Cole H <nightbat13131@gmail.com>
2026-05-15 15:59:47 -0700

    Add and use some posters.

--- levels/bowling_pin_formation.gd
+++ levels/bowling_pin_formation.gd
@@ -6,7 +6,7 @@ var ball_index := 0
 var _point_pos : Vector2
 var _row_length := 0
 var _row_count := 5
-var _here_radis := (Ball.BALL_RADIUS * 2.1)
+var _here_radis := (Ball.BALL_RADIUS * 2.05)
 var _start_direction := Vector2.from_angle( 5* PI/-6.0) * _here_radis
 
 func _ready() -> void:
